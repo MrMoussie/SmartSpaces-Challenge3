2d864a133860db758038305cc2c51a54246f47ed
MrMoussie <maouheb.bessi@gmail.com>
2022-09-21 11:35:17 +0200

    Initial weka setup

--- app/src/main/java/com/group18/activityclassification/MainActivity.java
+++ app/src/main/java/com/group18/activityclassification/MainActivity.java
@@ -3,11 +3,24 @@ package com.group18.activityclassification;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import weka.classifiers.trees.J48;
+import weka.core.SerializationHelper;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadWekaModel();
+    }
+
+    private void loadWekaModel() {
+        try {
+            J48 tree = (J48) SerializationHelper.read("assets/J48.model");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
