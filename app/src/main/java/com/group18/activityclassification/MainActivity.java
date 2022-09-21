7292481d5f431485a385ff6d4ca81b1b47e5710d
MrMoussie <maouheb.bessi@gmail.com>
2022-09-21 13:13:18 +0200

    Weka dependency

--- app/src/main/java/com/group18/activityclassification/MainActivity.java
+++ app/src/main/java/com/group18/activityclassification/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 import java.io.InputStream;
 import weka.classifiers.functions.MultilayerPerceptron;
+import weka.classifiers.trees.J48;
 import weka.core.SerializationHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String FILE_J48 = "j48tree.model";
+    private final static String FILE_MP = "mp.model";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +22,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadWekaModel() {
-        try (InputStream stream = getAssets().open("mp.model")) {
-            MultilayerPerceptron tree = (MultilayerPerceptron) SerializationHelper.read(stream);
+        try (InputStream stream = getAssets().open(FILE_J48)) {
+            J48 tree = (J48) SerializationHelper.read(stream);
 
         } catch (Exception e) {
             e.printStackTrace();
